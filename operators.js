c2ba16c28f9e3a94241b32f94be842fb97cbd915
Azegza <136713537+Azegza@users.noreply.github.com>
2024-04-22 17:02:47 +0100

    Update operators.js
    added the += operator

--- operators.js
+++ operators.js
@@ -1,7 +1,7 @@
 //  the logical || operator
 var timeRemaining = 0;
 var energy = 10;
-console.log("Game Over :" + (timeRemaining == 10 || energy == 0)) ;
+console.log("Game Over :" + (timeRemaining == 10 || energy == 0)) ; //() needed in order to make things work :)
 
 // the modulus operator, %, to test if a given number is odd
 var num1 = 2;
@@ -12,3 +12,10 @@ var result1= (test1 == 0); // () needed to assign the result of the comparaison 
 var result2 = (test2 == 0); // () needed to assign the result of the comparaison (true/false)
 console.log("Is" +num1 +" an even number?" + result1);   // output : Is2 an even number?true
 console.log("Is" +num2 +" an even number?" + result2);   // output : Is5 an even number?false
+
+
+//  += operator to accumulate values in a variable4
+var counter = 0;
+counter += 5;
+counter +=3;
+console.log(counter); // output: 8 

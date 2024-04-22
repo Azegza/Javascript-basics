9c154da429feae7db53f8a627ed6b70fc8e617ed
Azegza <136713537+Azegza@users.noreply.github.com>
2024-04-22 16:54:20 +0100

    Update operators.js

--- operators.js
+++ operators.js
@@ -11,4 +11,4 @@ var test2 = num2 % 2;
 var result1= (test1 == 0); // () needed to assign the result of the comparaison (true/false)
 var result2 = (test2 == 0); // () needed to assign the result of the comparaison (true/false)
 console.log("Is" +num1 +" an even number?" + result1);   // output : Is2 an even number?true
-console.log("Is" +num1 +" an even number?" + result2);   // output : Is2 an even number?false
+console.log("Is" +num2 +" an even number?" + result2);   // output : Is5 an even number?false

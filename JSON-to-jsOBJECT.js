7baff1a2b2a3b3406f9582db7565b2565c35574e
Azegza <136713537+Azegza@users.noreply.github.com>
2024-05-04 15:30:20 +0100

    Create JSON-to-jsOBJECT.js

---
+++ JSON-to-jsOBJECT.js
@@ -0,0 +1,12 @@
+// convert a JSON to js object
+const jsonStr = '{"Name":"Joe","age":"24"}';
+JSON.parse(jsonStr);
+
+
+// convert a js object to JSON
+
+const data = {
+  name :"Alice" ,
+  age : 30,
+}
+JSON.stringify(data)
